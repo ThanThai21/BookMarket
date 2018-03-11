438bcf01410351d2b7b3b53c24901c6a9ca818f6
thaivt.tj <tj.thaitv@hblab.vn>
2018-03-11 15:43:21 +0700

    create book detail screen

--- BookMarket/app/src/main/java/com/esp/bookmarket/utils/AppUtils.java
+++ BookMarket/app/src/main/java/com/esp/bookmarket/utils/AppUtils.java
@@ -1,9 +1,15 @@
 package com.esp.bookmarket.utils;
 
+import android.text.TextUtils;
+import android.util.Patterns;
+
 public class AppUtils {
 
     public static boolean checkValidString(String str) {
         return str != null && !str.isEmpty();
     }
 
+    public final static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
 }
